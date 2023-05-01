fdb382662c06d3b1ccf3669500b5c49f48506bac
Sam Jones <sam.jones240699@gmail.com>
2023-05-01 09:40:46 +0100

    Add project files.

---
+++ Viewfinder-DebugMenu/DebugMenuMod.cs
@@ -0,0 +1,36 @@
+﻿using Il2Cpp;
+using MelonLoader;
+using UnityEngine;
+
+namespace Viewfinder_DebugMenu
+{
+    public class DebugMenuMod: MelonMod
+    {
+        private bool isDebugMenuActive = false;
+        public override void OnLateUpdate()
+        {
+            GameObject debugMenuParent = GameObject.Find("Debug Menu");
+            if (debugMenuParent == null) return;
+
+            GameObject debugMenu = debugMenuParent.transform.GetChild(0).gameObject;
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                if (!isDebugMenuActive)
+                {
+                    debugMenu.SetActive(true);
+                }
+                isDebugMenuActive = !isDebugMenuActive;
+            }
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                DebugOverlayController controller = GameObject.Find("Debug Overlay").GetComponent<DebugOverlayController>();
+                controller.SetOpen(!controller.IsOpen());
+            }
+            if (Input.GetKeyDown(KeyCode.F3))
+            {
+                DebugFlyMode controller = debugMenuParent.GetComponent<DebugFlyingToggle>().debugFlyMode;
+                controller.SetFlyingEnabled(!controller.GetFlyingEnabled());
+            }
+        }
+    }
+}
